561dbca2fd285fd30d3d112e216f1d6c19256d42
Diana Barsan <barsan@medicmobile.org>
2018-05-21 14:02:58 +0300

    Deleted bugfix and grunt actually delete medic-test-audit!

--- Gruntfile.js
+++ Gruntfile.js
@@ -371,7 +371,7 @@ module.exports = function(grunt) {
       },
       resetTestDatabases: {
         stderr: false,
-        cmd: ['medic-test', 'medic-audit-test']
+        cmd: ['medic-test', 'medic-test-audit']
                 .map(name => `curl -X DELETE http://admin:pass@localhost:5984/${name}`)
                 .join(' && ')
       },
